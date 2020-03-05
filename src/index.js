72c3ffa404bd903989675de275b8491a51a8f8df
alexger95 <alexger95@gmail.com>
2020-03-05 22:24:02 +0300

    init: v1 solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    let indexO = [];
+    let indexC = [];
+
+    expr = expr.split('');
+    //проверка скобок
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] == '(') {
+            indexO.push(expr[i]);
+        }
+        if (expr[i] == ')') {
+            indexC.push(expr[i]);
+        }
+        if (expr[i] + expr[i + 1] + expr[i + 2] == "/ 0") throw ("TypeError: Division by zero.");
+    }
+    if (indexO.length != indexC.length) {
+        throw ("ExpressionError: Brackets must be paired");
+
+    }
+    expr = expr.join('').replace(/\s/g, "");
+
+    var value = expr.match(/(^[0-9*\/\\(\\)+-]+$)/);
+    var res = new Function('return ' + value);
+    if (res() == Infinity) throw ("TypeError: Division by zero.");
+
+    return res();
 }
 
 module.exports = {
